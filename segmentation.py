ce87f3b53aea6e1620f7098a6ad6a635515ba356
Lisa Tostrams <ltostrams@machine2learn.nl>
2019-05-14 23:36:53 +0200

    box detection TODO define rect,mask border

--- segmentation.py
+++ segmentation.py
@@ -20,19 +20,31 @@ from PIL import Image
 from skimage import measure
 from os import listdir
 from skimage.morphology import closing
+from scipy import ndimage
+
+from skimage import data
+from skimage.filters import threshold_otsu
+from skimage.segmentation import clear_border
+from skimage.measure import label, regionprops
+from skimage.morphology import closing, square
+from skimage.color import label2rgb
+#%%
+
 dirc = 'plaatjes'
+
+
 ls = listdir(dirc)
 ext = 'png jpg jpeg gif'
-ls = [l for l in ls if l[-3:] in ext]
+ls = [l for l in ls if l[-3:] in ext and 'mask' not in l]
 from statistics import mode
 #
-def mask_greens(img,greenthresh=100,redthresh=200,bluethresh=200):
+def mask_greens(img,greenthresh=90,redthresh=230,bluethresh=230):
     green = img[:,:,1]>greenthresh
     not_red = img[:,:,0] < redthresh
     not_blue = img[:,:,2] < bluethresh
-    diffrg = abs(img[:,:,0] - img[:,:,1]) > 40
-    diffgb = abs(img[:,:,1] - img[:,:,2]) < 220
-    diffrb = abs(img[:,:,0] - img[:,:,2]) < 220
+    diffrg = abs(img[:,:,0] - img[:,:,1]) > 30
+    diffgb = abs(img[:,:,1] - img[:,:,2]) < 230
+    diffrb = abs(img[:,:,0] - img[:,:,2]) < 230
     color_less  = np.logical_and(diffrb,np.logical_and(diffrg,diffgb))
     return np.logical_and(np.logical_and(np.logical_and(green,not_red),not_blue),color_less)
     
@@ -44,42 +56,88 @@ for f in ls:
     r,c,_ = img1.shape
 
     imgs.append(img1)
-    
-    fig, ax = plt.subplots(1, 2,figsize=(12,6))
-    ax[0].imshow(img1,cmap='gray')
-    ax[1].hist(img1[:,:,1].ravel(), bins=255, range=[0, 256])
-    ax[1].set_title('Green channel distribution')
-    ax[1].set_xlim(0, 256);
+    #img1 = ndimage.gaussian_filter(img1,0.4)
+    fig, ax = plt.subplots(2, 2,figsize=(24,12))
+    ax[0,0].imshow(img1,cmap='gray')
+    ax[0,1].hist(img1[:,:,1].ravel(), bins=255, range=[0, 256])
+    ax[0,1].set_title('Green channel distribution')
+    ax[0,1].set_xlim(0, 256);
+    twigs = np.sum(img1[:,:]>245,axis=2)
+    ax[1,0].imshow(closing(twigs))
+    xs,ys = np.where(twigs>1)
     plt.show()
     greens = closing(mask_greens(img1,))
     plt.imshow(greens)
-
+    
     pct = sum(greens.ravel())/(r*c)
     plt.title('{:.4f} percent of the image is plant'.format(pct*100))
     plt.savefig('plaatjes/masked_'+f)
-##
-#    
-#def circle_points(resolution, center, radius):
-#    """
-#    Generate points which define a circle on an image.Centre refers to the centre of the circle
-#    """   
-#    radians = np.linspace(0, 2*np.pi, resolution)
-#    c = center[1] + radius*np.cos(radians)#polar co-ordinates
-#    r = center[0] + radius*np.sin(radians)
-#    
-#    return np.array([c, r]).T
-## Exclude last point because a closed path should not have duplicate points
-#cat = circle_points(200, [720, 250], 280)[:-1]
-#human = circle_points(200,[700,750],280)[:-1]    
-#renske = imgs[5]
-#fig,ax = plt.subplots(1,1,figsize=(8,5))
-#plt.imshow(renske)
-#image_gray = color.rgb2gray(renske) 
-#plt.imshow(renske)
-#plt.plot(cat[:, 0], cat[:, 1], '--r', lw=2)
-#plt.plot(human[:, 0], human[:, 1], '--g', lw=2)
-#snake = seg.active_contour(image_gray, cat,alpha=0.002,beta=0.1)
-#snake_hum = seg.active_contour(image_gray,human,alpha=0.002,beta=0.1)
-#plt.plot(snake[:, 0], snake[:, 1], '-b', lw=1);
-#plt.plot(snake_hum[:, 0], snake_hum[:, 1], '-b', lw=1);
+#%%
+from skimage.feature import corner_harris, corner_subpix, corner_peaks
+import matplotlib.patches as mpatches
 
+
+
+from skimage.transform import (hough_line, hough_line_peaks,
+                               probabilistic_hough_line)
+from skimage.feature import canny
+h, theta, d = hough_line(image)
+# Generating figure 1
+fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+ax = axes.ravel()
+
+ax[0].imshow(image, cmap=plt.cm.gray)
+ax[0].set_title('Input image')
+ax[0].set_axis_off()
+
+
+
+ax[1].imshow(image, cmap=plt.cm.gray)
+for some, angle, dist in zip(*hough_line_peaks(h, theta, d)):
+    y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
+    y1 = (dist - image.shape[1] * np.cos(angle)) / np.sin(angle)
+    ax[1].plot((0, image.shape[1]), (y0, y1), '-r')
+ax[1].set_xlim((0, image.shape[1]))
+ax[1].set_ylim((image.shape[0], 0))
+ax[1].set_axis_off()
+ax[1].set_title('Detected lines')
+
+plt.tight_layout()
+plt.show()
+
+#thresh = threshold_otsu(image)
+#bw = closing(image > thresh, square(1))
+#plt.imshow(bw)\
+#contours = measure.find_contours(r, 0.1)
+#
+## Display the image and plot all contours found
+#fig, ax = plt.subplots()
+#ax.imshow(r, interpolation='nearest', cmap=plt.cm.gray)
+#
+#for n, contour in enumerate(contours):
+#    ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
+
+# remove artifacts connected to image border
+#cleared = clear_border(bw)
+#imshow(cleared)
+
+
+# label image regions
+#label_image = label(cleared)
+#image_label_overlay = label2rgb(label_image, image=image)
+#
+#fig, ax = plt.subplots(figsize=(10, 6))
+#ax.imshow(image_label_overlay)
+#
+#for region in regionprops(label_image):
+#    # take regions with large enough areas
+#    if region.area >= 100:
+#        # draw rectangle around segmented coins
+#        minr, minc, maxr, maxc = region.bbox
+#        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+#                                  fill=False, edgecolor='red', linewidth=2)
+#        ax.add_patch(rect)
+#
+#ax.set_axis_off()
+#plt.tight_layout()
+#plt.show()
