45d59402f46d51bdc8ee94192e31d8d6d17b3ef7
Lisa Tostrams <ltostrams@machine2learn.nl>
2019-05-21 17:20:22 +0200

    clean some random stuff

--- segmentation.py
+++ segmentation.py
@@ -29,7 +29,6 @@ from skimage.measure import label, regionprops
 from skimage.morphology import closing, square
 from skimage.color import label2rgb
 import matplotlib.patches as mpatches
-#%%
 
 dirc = 'plaatjes'
 
@@ -92,13 +91,9 @@ for f in ls:
     r,c,_ = img1.shape
 
     imgs.append(img1)
-    #img1 = ndimage.gaussian_filter(img1,0.4)
-    
+
     fig, ax = plt.subplots(2, 2,figsize=(18,12))
     ax[0,0].imshow(img1,cmap='gray')
-    #ax[0,1].hist(img1[:,:,1].ravel(), bins=255, range=[0, 256])
-    #ax[0,1].set_title('Green channel distribution')
-    #ax[0,1].set_xlim(0, 256);
     twigs = np.sum(img1[:,:]>235,axis=2)
 
     ax[0,1].imshow(closing(twigs))
@@ -129,78 +124,3 @@ for f in ls:
     plt.title('{:.2f}% percent of the box is plant'.format(pct*100))
     plt.tight_layout()
     plt.savefig('plaatjes/masked_'+f,dpi=300)
-#%%
-#from skimage.feature import corner_harris, corner_subpix, corner_peaks
-#import matplotlib.patches as mpatches
-#
-#
-#
-#from skimage.transform import (hough_line, hough_line_peaks,
-#                               probabilistic_hough_line)
-#from skimage.feature import canny
-#h, theta, d = hough_line(image)
-## Generating figure 1
-#fig, axes = plt.subplots(1, 2, figsize=(15, 6))
-#ax = axes.ravel()
-#
-#ax[0].imshow(image, cmap=plt.cm.gray)
-#ax[0].set_title('Input image')
-#ax[0].set_axis_off()
-#from skimage.draw import line
-#
-#
-#
-#ax[1].imshow(image, cmap=plt.cm.gray)
-#for some, angle, dist in zip(*hough_line_peaks(h, theta, d)):
-#    print(angle)
-#    if((angle>-0.1 and angle < .1) or (angle>1.5 and angle<1.8)):
-#        y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
-#        y1 = (dist - image.shape[1] * np.cos(angle)) / np.sin(angle)
-#        ax[1].plot((0, image.shape[1]), (y0, y1), '-r')
-#ax[1].set_xlim((0, image.shape[1]))
-#ax[1].set_ylim((image.shape[0], 0))
-#ax[1].set_axis_off()
-#ax[1].set_title('Detected lines')
-#
-#plt.tight_layout()
-#plt.show()
-#
-#thresh = threshold_otsu(image)
-#bw = closing(image > thresh, square(1))
-##bw= closing(bw,square(3))
-#plt.imshow(bw)
-##contours = measure.find_contours(r, 0.1)
-##
-### Display the image and plot all contours found
-##fig, ax = plt.subplots()
-##ax.imshow(r, interpolation='nearest', cmap=plt.cm.gray)
-##
-##for n, contour in enumerate(contours):
-##    ax.plot(contour[:, 1], contour[:, 0], linewidth=2)
-##
-##
-## #remove artifacts connected to image border
-#cleared = clear_border(bw)
-#imshow(cleared)
-#
-#
-## label image regions
-#label_image = label(cleared)
-#image_label_overlay = label2rgb(label_image, image=image)
-#
-#fig, ax = plt.subplots(figsize=(10, 6))
-#ax.imshow(image_label_overlay)
-#
-#for region in regionprops(label_image):
-#    # take regions with large enough areas
-#    if region.area >= 4000:
-#        print(region.area)
-#        # draw rectangle around segmented coins
-#        minr, minc, maxr, maxc = region.bbox
-#        rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-#                                  fill=False, edgecolor='red', linewidth=2)
-#        ax.add_patch(rect)
-#
-#ax.set_axis_off()
-#plt.tight_layout()
-#plt.show()
